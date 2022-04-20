20157f80aa5478c61dec6c7e0c114dbb8ab64ec1
Emir Guvenni <contact@emirguvenni.com>
2022-04-21 02:51:08 +0300

    Removed Unnecessary stuff

--- src/util.ts
+++ src/util.ts
@@ -1,13 +1,5 @@
 import { dirname, resolve } from 'path';
 
-/*
-"baseUrl": ".",
-"outDir": "lib",
-"paths": {
-  "src/*": ["src/*"]
-},
-*/
-
 export interface IRawTSConfig {
   extends?: string;
   compilerOptions?: {
@@ -23,17 +15,6 @@ export interface ITSConfig {
   paths?: { [key: string]: string[] };
 }
 
-export const mapPaths = (
-  paths: { [key: string]: string[] },
-  mapper: (x: string) => string
-): { [key: string]: string[] } => {
-  const dest = {} as { [key: string]: string[] };
-  Object.keys(paths).forEach((key) => {
-    dest[key] = paths[key].map(mapper);
-  });
-  return dest;
-};
-
 export const loadConfig = (file: string): ITSConfig => {
   const {
     extends: ext,
@@ -45,15 +26,10 @@ export const loadConfig = (file: string): ITSConfig => {
   } = require(file) as IRawTSConfig;
 
   const config: ITSConfig = {};
-  if (baseUrl) {
-    config.baseUrl = baseUrl;
-  }
-  if (outDir) {
-    config.outDir = outDir;
-  }
-  if (paths) {
-    config.paths = paths;
-  }
+
+  if (baseUrl) config.baseUrl = baseUrl;
+  if (outDir) config.outDir = outDir;
+  if (paths) config.paths = paths;
 
   if (ext) {
     const parentConfig = loadConfig(resolve(dirname(file), ext));
